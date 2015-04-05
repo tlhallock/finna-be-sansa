aceed1e9c214f2a4bea631e770afeb2de67a2faf
tlhallock <theboxforspam@gmail.com>
2015-04-04 20:29:12 -0600

    again

--- src/run.cpp
+++ src/run.cpp
@@ -48,7 +48,6 @@ int main(int argc, char *argv[])
         std::this_thread::sleep_for(std::chrono::milliseconds{20});
     }
 
-    getLog() << "we get here" << std::endl;
     gfx::destroyScene();
     closeLoggin();
 
